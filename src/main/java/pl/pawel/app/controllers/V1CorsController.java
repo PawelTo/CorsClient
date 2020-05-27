02f2146d700bc466f196c5b2d5be48e24c9e1449
PawelTo <pawel.toporowicz@gmail.com>
2020-05-27 02:06:53 +0200

    create localhost consumer

--- src/main/java/pl/pawel/app/controllers/V1CorsController.java
+++ src/main/java/pl/pawel/app/controllers/V1CorsController.java
@@ -1,7 +1,12 @@
 package pl.pawel.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pawel.app.models.CorsRecivedModel;
+import pl.pawel.app.services.LocalHostCaller;
+import pl.pawel.app.services.RestCaller;
 
 @RequestMapping(V1CorsController.API_PAHT)
 @RestController
@@ -9,5 +14,13 @@ public class V1CorsController {
 
     static final String API_PAHT = "/cors-server";
 
+    @Autowired
+    private LocalHostCaller localHostCaller;
 
+    @GetMapping()
+    public CorsRecivedModel getForDataFromAnotherService(){
+        CorsRecivedModel corsRecivedModel = localHostCaller.getForObject();
+        System.out.println("Recived Model: "+corsRecivedModel);
+        return corsRecivedModel;
+    }
 }
---
+++ src/main/java/pl/pawel/app/services/LocalHostCaller.java
@@ -0,0 +1,21 @@
+package pl.pawel.app.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import pl.pawel.app.models.CorsRecivedModel;
+
+@Component
+public class LocalHostCaller implements RestCaller{
+
+    private final String RESOURCE_URL = "http://localhost:8080/cors-server";
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    public CorsRecivedModel getForObject(){
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        return restTemplate.getForObject(RESOURCE_URL, CorsRecivedModel.class);
+    }
+}
